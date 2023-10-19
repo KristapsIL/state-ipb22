3e21e9a3a27d3fe86a9c21c6d5fb1bcdbf987f9a
Skolnieks <skolnieks.pjvt@pjvt.local>
2023-10-19 09:48:25 +0300

    ahhh

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import {useState } from "react";
 function App() {
-  const [result, setResult] = useState(0)
+  const [result, setResult] = useState(0);
+  const [ratioChange, setRatioChange] = useState(1);
+
   function addOne(){
-    setResult(result+1);
+    setResult(result+parseInt(ratioChange));
     console.log(result);
   }
   function minusOne(){
-    setResult(result-1);
+    setResult(result-ratioChange);
+  }
+  function handleRatioChange(quak){
+    setRatioChange(quak.target.value);
   }
   return (
     <div className="App">
       <form>
-        <input type="number" name="result "></input>
-        <input type="submit" value="Submit" />
+        <input type="number" value={ratioChange} onChange={handleRatioChange}></input>
       </form>
       <button onClick={addOne}>button +1</button>
       <button onClick={minusOne}>button -1</button>
